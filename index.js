3c7ae64eea3ff3701eab625283360c6095ac1226
Julian1729 <hernandez.julian17@gmail.com>
2019-12-11 22:30:19 -0500

    Add await

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const database = require(`./models/db`);
 const streetHundredSchema = require(`./schemas/street-hundred`);
 
 exports.connect = async () => {
-  const db = database.init();
+  const db = await database.init();
   return db.model('StreetHundred', streetHundredSchema);
 };
